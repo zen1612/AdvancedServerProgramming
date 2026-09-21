8d56aed3d04b845a626897ba9d2a7cc035c0c367
zen1612 <zenkhoo16@gmail.com>
2026-09-21 23:42:20 +0900

    Create HelloServerApplication.java

---
+++ HelloServerApplication.java
@@ -0,0 +1,11 @@
+package com.dsu.helloserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloServerApplication.class, args);
+    }
+}
